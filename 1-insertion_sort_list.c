a788037267816ec72f96ca2b4e6fefef7d80de32
root <root@MrSlim3.>
2024-04-08 07:19:47 -0500

    sort insertion algo

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,50 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending order using Insertion sort
+ * @list: Pointer to a pointer to the head of the list
+ */
+void insertion_sort_list(listint_t **list)
+{
+    listint_t *current_node, *swap_node, *previous_node;
+
+    if (!list || !*list)
+        return;
+
+    current_node = (*list)->next;
+
+    while (current_node)
+    {
+        swap_node = current_node;
+
+        while (swap_node->prev && swap_node->n < swap_node->prev->n)
+        {
+            previous_node = swap_node->prev;
+
+            /* Swap the nodes */
+            if (previous_node->prev)
+                previous_node->prev->next = swap_node;
+            if (swap_node->next)
+                swap_node->next->prev = previous_node;
+
+            previous_node->next = swap_node->next;
+            swap_node->prev = previous_node->prev;
+
+            swap_node->next = previous_node;
+            previous_node->prev = swap_node;
+
+            if (!swap_node->prev)
+                *list = swap_node;
+
+            print_list(*list);
+
+            if (previous_node->prev)
+                swap_node = previous_node->prev;
+            else
+                break;
+        }
+
+        current_node = current_node->next;
+    }
+}
+
--- sort.h
+++ sort.h
@@ -19,5 +19,8 @@ typedef struct listint_s
 /* Function prototypes */
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void print_list(const listint_t *list);
+listint_t *create_listint(const int *array, size_t size);
 
 #endif /* MY_HEADER_H */
