35216f64e4229f6047913a1c5aec2ec27c0b8fdb
root <root@MrSlim3.>
2024-04-08 08:15:58 -0500

    quick sort algo

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -25,18 +25,19 @@ int lomuto_partition(int *array, int low, int high, size_t size)
 {
     int pivot = array[high];
     int i = low - 1;
+    int j;
 
-    for (int j = low; j <= high - 1; j++)
+    for (j = low; j <= high - 1; j++)
     {
         if (array[j] < pivot)
         {
             i++;
             swap_int(&array[i], &array[j]);
-            print_array(array, size);  // Print the array after each swap
+            print_array(array, size);
         }
     }
     swap_int(&array[i + 1], &array[high]);
-    print_array(array, size);  // Print the array after each swap
+    print_array(array, size);
 
     return (i + 1);
 }
