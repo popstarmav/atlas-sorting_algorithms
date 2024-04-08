145f90df0da73d8d0b4802a024aeff8135b99240
root <root@MrSlim3.>
2024-04-08 00:20:07 -0500

    sort algo

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,41 +5,28 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t k;
 	size_t i, j;
 	int temp;
-	bool swapped = false;
-	
-	for (i = 0; i < size - 1; i++) 
+	bool swapped;
+
+	for (i = 0; i < size - 1; i++)
 	{
-		swapped = false;
-		
-		for (j = 0; j < size - 1 - i; j++)
+		swapped = 0;
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[j] > array[j+1])
+			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
-				array[j] = array[j+1];
-				array[j+1] = temp;
-				swapped = true;
-				
-				k = 0;
-				if (k < size)
-				{
-					printf("%d ", array[k]);
-					k++;
-				}
-				for(; k < size; k++)
-				{
-					printf("%d ", array[k]);
-				}
-				printf("\n");
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+
+				swapped = 1;
+				print_array(array, size);
 			}
 		}
-		if (!swapped)
-		{
+		
+		if (swapped == 0)
 			break;
-		}
 	}
 }
 
