6694c1b57cc586dc387550b403a835c8d7dcdc3c
root <root@MrSlim3.>
2024-04-08 23:44:43 -0500

    insertion_sort_list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,50 +1,52 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending order using Insertion sort
+ * insertion_sort_list - Sorts a doubly linked list of
+ * integers in ascending order using Insertion sort
  * @list: Pointer to a pointer to the head of the list
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current_node, *swap_node, *previous_node;
+	listint_t *node, *swap_node, *previous_node;
 
-    if (!list || !*list)
-        return;
+	if (!list || !*list)
+		return;
 
-    current_node = (*list)->next;
+	node = (*list)->next;
 
-    while (current_node)
-    {
-        swap_node = current_node;
+	while (current_node)
+	{
+		swap_node = current_node;
 
-        while (swap_node->prev && swap_node->n < swap_node->prev->n)
-        {
-            previous_node = swap_node->prev;
+		while (swap_node->prev && swap_node->n < swap_node->prev->n)
+		{
+			previous_node = swap_node->prev;
+			/* Swap the nodes */
 
-            /* Swap the nodes */
-            if (previous_node->prev)
-                previous_node->prev->next = swap_node;
-            if (swap_node->next)
-                swap_node->next->prev = previous_node;
+			if (previous_node->prev)
 
-            previous_node->next = swap_node->next;
-            swap_node->prev = previous_node->prev;
+			previous_node->prev->next = swap_node;
 
-            swap_node->next = previous_node;
-            previous_node->prev = swap_node;
+			if (swap_node->next)
 
-            if (!swap_node->prev)
-                *list = swap_node;
+			swap_node->next->prev = previous_node;
+			previous_node->next = swap_node->next;
+			swap_node->prev = previous_node->prev;
+			swap_node->next = previous_node;
+			previous_node->prev = swap_node;
 
-            print_list(*list);
+			if (!swap_node->prev)
 
-            if (previous_node->prev)
-                swap_node = previous_node->prev;
-            else
-                break;
-        }
+			*list = swap_node;
+			print_list(*list);
 
-        current_node = current_node->next;
-    }
+			if (previous_node->prev)
+			swap_node = previous_node->prev;
+
+			else
+				break;
+		}
+		node = node->next;
+	}
 }
 
