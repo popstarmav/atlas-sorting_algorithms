1fd0119cfdc1db9144967971f321572e4e1fc496
popstarmav <cj.johnson2017@gmail.com>
2025-02-04 22:37:18 -0600

    sorts an array of integers

--- sort.h
+++ sort.h
@@ -28,5 +28,6 @@ void quick_sort(int *array, size_t size);
 int lomuto_partition(int *array, int low, int high, size_t size);
 void quicksort(int *array, int low, int high, size_t size);
 listint_t *create_listint(const int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif /* MY_HEADER_H */
